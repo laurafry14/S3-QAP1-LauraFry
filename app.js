2741b74d66487382e1b726236ccfbfbc875a8895
Laura Fry <laura.fry@mun.ca>
2023-05-15 14:33:35 -0230

    Task2 complete

--- app.js
+++ app.js
@@ -1,2 +1,7 @@
-var msg = "Hello World";
+// Task 2: NODEMON
+// Type nodemon into the terminal to use
+
+var msg = "Hello World!";
 console.log(msg);
+
+console.log("You have successfully used nodemon");
